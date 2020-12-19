16bb5dae1dfe584edc9be648143a8d6ab10e7840
www0402www <www0402www@gmail.com>
2020-12-19 13:43:45 +0800

    adding resource

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,2 +1,14 @@
-public class GameClient {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+    private  int screenWidth;
+    private int screenHeight;
+
+    public GameClient(int screenWidth,int screenHeight){
+        this.screenWidth=screenWidth;
+        this.screenHeight=screenHeight;
+
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+    }
 }
--- src/main/java/TankGame.java
+++ src/main/java/TankGame.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class TankGame {
+    public static void main(String[] args) {
+        GameClient gameClient=new GameClient(800,600);
+        JFrame frame=new JFrame();
+        frame.add(gameClient);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+    }
 }
